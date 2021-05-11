3bb6bbcd7fcf6bce6e155c1099d15201945da0d9
mccleskeyc <mccleskeyc@gmail.com>
2021-05-11 14:17:00 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <Header />
-      <Home />
+      <Switch>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/detail">
+          <Detail />
+        </Route>
+
+      </Switch>
+    </Router>
     </div>
   );
 }
