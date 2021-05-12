c8fbfddc91906441c45f0c82caa87cccfba0a635
mccleskeyc <mccleskeyc@gmail.com>
2021-05-12 10:28:30 -0400

    added dynamic data to detail pages

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,44 +1,67 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { useParams } from "react-router-dom";
+import db from "../firebase";
+import { useHistory } from 'react-router-dom';
+
+
 
 function Detail() {
+    const { id } = useParams();
+    const history = useHistory();
+    const [ movie, setMovie ] = useState();
+
+    useEffect(()=>{
+        db.collection("movies")
+        .doc(id)
+        .get()
+        .then((doc)=>{
+            if(doc.exists) {
+                setMovie(doc.data());
+                console.log(doc.data());
+            } else {
+                history.push('/')
+            }
+        })
+    }, [])
+
     return (
         <Container>
+        {movie && (
+            <>
             <Background>
-                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" alt="Bao"/>
+                <img src={movie.backgroundImg} />
             </Background>
-
+            
             <ImageTitle>
-                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" alt="Disney Pixar's Bao"/>
-            </ImageTitle>  
-
-            <Controls >
+                <img src={movie.titleImg} />
+            </ImageTitle>
+            
+            <Controls>
                 <PlayButton>
-                    <img src="/images/play-icon-black.png" alt="Play" />
+                    <img src="/images/play-icon-black.png" />
                     <span>PLAY</span>
                 </PlayButton>
-
                 <TrailerButton>
-                    <img src="/images/play-icon-white.png" alt="Play" />
-                    <span>TRAILER</span>
+                    <img src="/images/play-icon-white.png" />
+                    <span>Trailer</span>
                 </TrailerButton>
-
                 <AddButton>
                     <span>+</span>
                 </AddButton>
-
                 <GroupWatchButton>
-                    <img src="/images/group-icon.png" alt="Group Watch" />
+                    <img src="/images/group-icon.png" />
                 </GroupWatchButton>
-            </Controls>   
-
+            </Controls>
             <Subtitle>
-                2018 • 7m • Family, Fantasy, Kids, Animation
-            </Subtitle>   
-
+                {movie.subTitle}
+                </Subtitle>
             <Description>
-                A Chinese mom who’s sad when her grown son leaves home gets another chance at motherhood when one of her dumplings springs to life. But she finds that nothing stays cute and small forever.
-            </Description>    
+                {movie.description}
+            </Description> 
+            </>
+        )}
+             
         </Container>
     )
 }
